299c6f068333f9fc6b06b53ec9be764b4c85487b
Nick McDonnough <nickmcd@gmail.com>
2014-07-19 21:42:06 -0500

    Typo in project.clj

--- project.clj
+++ project.clj
@@ -9,8 +9,7 @@
                  [org.clojure/data.json "0.2.5"]
                  [environ "0.5.0"]]
   :plugins [[lein-ring "0.8.11"]]
-  :ring {:handler techno-bot.handler/app
-         :init}
+  :ring {:handler techno-bot.handler/app}
   :main techno-bot.handler
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
