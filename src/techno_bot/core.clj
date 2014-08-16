097f186cc6f2f5168a4ccc0ef09d5792a946c8aa
John Hume <duelin.markers@gmail.com>
2014-08-16 13:40:54 -0500

    Multimethod instead of String->Fn map.

--- src/techno_bot/core.clj
+++ src/techno_bot/core.clj
@@ -23,18 +23,27 @@
                                        :icon_emoji ":de:"
                                        :text message})}))
 
-(def user-exec {"youtube" (fn [user search-terms]
-                            (-> search-terms
-                                yt/get-youtube-data
-                                (assoc :user user)
-                                build-bot-message
-                                post-to-slack))
-                "weather" (fn [& _] (post-to-slack (weather/austin)))})
-                ;"what" #(-> %2 what/parse (assoc :user %1))})
+(defmulti do-command (fn [command _ _] command))
+
+(defmethod do-command :default [command user _]
+  (post-to-slack "Sorry, " user ", I don't know about " command "."))
+
+(defmethod do-command "youtube" [_ user search-terms]
+  (-> search-terms
+      yt/get-youtube-data
+      (assoc :user user)
+      build-bot-message
+      post-to-slack))
+
+(defmethod do-command "weather" [& _]
+  (post-to-slack (weather/austin)))
+
+;; (defmethod do-command "what" [_ user search-terms]
+;;   (-> search-terms what/parse (assoc :user user)))
 
 (defn exec-user-command [{:keys [user text]}]
   (let [[_ command & search-terms] (string/split text #"\s")]
-    ((user-exec command) user search-terms)))
+    (do-command command user search-terms)))
 
 (defn process-incoming-webhook [username text]
   (exec-user-command {:user username :text text}))
